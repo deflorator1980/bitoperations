ab82ce9a5d4f672f0eebc7c08d4351c85809c6c0
Your Name <you@example.com>
2017-11-19 00:47:26 +0300

    a bit more correct naming

--- src/main/java/h/App.java
+++ src/main/java/h/App.java
@@ -9,34 +9,34 @@ import java.util.stream.IntStream;
  */
 public class App {
     private final static int QUANTITY = 4;
-    private final static int NUMBER = 15; // Integer.parseInt("1111",2)
+    private final static int NUMBER_BIN = 15; // Integer.parseInt("1111",2)
 
     public static void main(String[] args) {
         int sum0 = 0, sum1 = 0;
         int[] arr = new int[QUANTITY];
-        int[] res = new int[NUMBER];            //
+        int[] res = new int[NUMBER_BIN];            //
         for (int i = 0; i < QUANTITY; i++) {
             arr[i] = new Random().nextInt(10);
         }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        for (int n = 1; n <= NUMBER; n++) {
+        for (int n_b = 1; n_b <= NUMBER_BIN; n_b++) {
 
-            for (int b = 0; b < arr.length; b++) {
-                if (getBit(n, b) == 0) {
-                    sum0 += arr[b];
-                } else if (getBit(n, b) == 1) {
-                    sum1 += arr[b];
+            for (int n_d = 0; n_d < arr.length; n_d++) {
+                if (getBit(n_b, n_d) == 0) {
+                    sum0 += arr[n_d];
+                } else if (getBit(n_b, n_d) == 1) {
+                    sum1 += arr[n_d];
                 }
             }
-            res[n-1] = Math.abs(sum1 - sum0);
+            res[n_b-1] = Math.abs(sum1 - sum0);
             sum0 = sum1 = 0;
         }
         System.out.println("\n" + IntStream.of(res).min().getAsInt());
     }
 
-    public static int getBit(int n, int b) {
-        return (n >> b) & 1;
+    public static int getBit(int n_b, int n_d) {
+        return (n_b >> n_d) & 1;
     }
 }
