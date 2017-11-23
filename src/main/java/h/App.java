1df8ae9e4d3de7334f383d88f7b7b29ff9b7d7c0
Your Name <you@example.com>
2017-11-23 23:17:48 +0300

    scala loops

--- src/main/java/h/App.java
+++ src/main/java/h/App.java
@@ -4,39 +4,39 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 /**
- * Решаем перебором. Длину массива -- в двоичное представление: 0 -первая группа
- * 1 - вторая
+ * Решаем перебором. Длинa массива -- количество бит -- количество комбинаций
+ * -- в десятичное представление: 0 -первая группа, 1 - вторая
  */
 public class App {
     private final static int QUANTITY = 4;
-    private final static int NUMBER_BIN = 15; // Integer.parseInt("1111",2)
+    private final static int NUMBER_DEC = 15; // Integer.parseInt("1111",2)
 
     public static void main(String[] args) {
         int sum0 = 0, sum1 = 0;
         int[] arr = new int[QUANTITY];
-        int[] res = new int[NUMBER_BIN];            //
+        int[] res = new int[NUMBER_DEC];            //
         for (int i = 0; i < QUANTITY; i++) {
             arr[i] = new Random().nextInt(10);
         }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        for (int n_b = 1; n_b <= NUMBER_BIN; n_b++) {
+        for (int n_dec = 1; n_dec <= NUMBER_DEC; n_dec++) {
 
-            for (int n_d = 0; n_d < arr.length; n_d++) {
-                if (getBit(n_b, n_d) == 0) {
-                    sum0 += arr[n_d];
-                } else if (getBit(n_b, n_d) == 1) {
-                    sum1 += arr[n_d];
+            for (int n_bin = 0; n_bin < arr.length; n_bin++) {
+                if (getBit(n_dec, n_bin) == 0) {
+                    sum0 += arr[n_bin];
+                } else if (getBit(n_dec, n_bin) == 1) {
+                    sum1 += arr[n_bin];
                 }
             }
-            res[n_b-1] = Math.abs(sum1 - sum0);
+            res[n_dec-1] = Math.abs(sum1 - sum0);
             sum0 = sum1 = 0;
         }
         System.out.println("\n" + IntStream.of(res).min().getAsInt());
     }
 
-    public static int getBit(int n_b, int n_d) {
-        return (n_b >> n_d) & 1;
+    public static int getBit(int n_dec, int n_bin) {
+        return (n_dec >> n_bin) & 1;
     }
 }
--- src/main/scala/AppS.scala
+++ src/main/scala/AppS.scala
@@ -6,10 +6,15 @@ object AppS {
     println(getBit(1, 0))
     val arr = Array.fill(quantity){scala.util.Random.nextInt(10)}
     arr.foreach({a => print(a + " ")})
+
+    /**
+      * todo for(i <- 1 until arr.length) {println(arr(i))
+      */
   }
 
   def getBit (n_b: Int, n_d: Int) =  {
     val x = (n_b >> n_d) & 1
-    x + quantity
+    x
   }
 }
+//todo look up
