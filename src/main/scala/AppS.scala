e5de886ba345ea00125bcb8f8d6509207387c235
Your Name <you@example.com>
2017-11-24 00:07:59 +0300

    scala solving

--- src/main/scala/AppS.scala
+++ src/main/scala/AppS.scala
@@ -1,15 +1,24 @@
 object AppS {
   val quantity = 4
-  val number_bin = 15
+  val number_dec = 15
   def main(args: Array[String]) {
     var sum0, sum1 = 0
-    println(getBit(1, 0))
     val arr = Array.fill(quantity){scala.util.Random.nextInt(10)}
+    val res = new Array[Int](number_dec)
     arr.foreach({a => print(a + " ")})
 
-    /**
-      * todo for(i <- 1 until arr.length) {println(arr(i))
-      */
+    for (n_dec <- 1 to number_dec) {
+      for (n_bin <- 0 until arr.length) {
+        if (getBit(n_dec, n_bin) == 0) {
+          sum0 += arr(n_bin)
+        } else if (getBit(n_dec, n_bin) == 1) {
+          sum1 += arr(n_bin)
+        }
+      }
+      res(n_dec-1) = Math.abs(sum1 - sum0)
+      sum0 = 0; sum1 = 0
+    }
+    println("\n" + res.reduce(_ min _))
   }
 
   def getBit (n_b: Int, n_d: Int) =  {
@@ -17,4 +26,3 @@ object AppS {
     x
   }
 }
-//todo look up
