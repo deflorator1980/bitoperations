dee179b0c16cde1ec5da20840916fe512cc3854e
Andrey Isakov <a.isakov@nelant.ru>
2018-02-06 17:29:24 +0300

    start refactor

--- src/main/java/AppKnapSack.java
+++ src/main/java/AppKnapSack.java
@@ -7,40 +7,33 @@ public class AppKnapSack {
 
         int quantity = Integer.parseInt(args[0]);
         long[] arr = new long[quantity];
-        long sum0 = 0, sum1 = 0;
         for (int i = 0; i < quantity; i++) {
             arr[i] = new Random().nextInt(10);
         }
 
         Arrays.stream(arr).forEach(value -> System.out.print(value + " "));
-        System.out.println();
-        long val[] = arr;
-        long wt[] = arr;
         int sum = (int) Arrays.stream(arr).reduce(0, Long::sum);
-        System.out.println("sum " + sum);
+        System.out.println("\nsum " + sum);
         int W = sum / 2;
         System.out.println("W " + W);
-        int n = val.length;
-        int one = knapSack(W, wt, val, n);
+        int n = arr.length;
+        int one = knapSack(W, arr, n);
         System.out.println("one " + one);
         System.out.println("result " + (sum - (one * 2)));
     }
 
-    static int knapSack(int W, long wt[], long val[], int n) {
-        int i, w;
+    static int knapSack(int W, long arr[], int n) {
         int K[][] = new int[n + 1][W + 1];
-
-        for (i = 0; i <= n; i++) {
-            for (w = 0; w <= W; w++) {
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
                 if (i == 0 || w == 0)
                     K[i][w] = 0;
-                else if (wt[i - 1] <= w)
-                    K[i][w] = (int) Math.max(val[i - 1] + K[i - 1][(int) (w - wt[i - 1])], K[i - 1][w]);
+                else if (arr[i - 1] <= w)
+                    K[i][w] = (int) Math.max(arr[i - 1] + K[i - 1][(int) (w - arr[i - 1])], K[i - 1][w]);
                 else
                     K[i][w] = K[i - 1][w];
             }
         }
-
         return K[n][W];
     }
 }
