fdd052c90a54ec114833a568c5af06b775265c64
Andrey Isakov <a.isakov@nelant.ru>
2018-02-06 13:07:09 +0300

    remove unused

--- src/main/java/AppCallLoop.java
+++ src/main/java/AppCallLoop.java
@@ -21,26 +21,17 @@ public class AppCallLoop {
             arr[i] = new Random().nextInt(10);
         }
 
-
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
 
         int cores = Runtime.getRuntime().availableProcessors();
 
-//        cores = cores*2;
-
         System.out.println("\n" + new Date());
 
         ExecutorService exec = Executors.newFixedThreadPool(cores);
 
-        // long part = numberDec/4;
         long part = numberDec/cores;
-
-        // Callable<Integer> callable = new CallCount(arr, 1, numberDec);
-        // Callable<Integer> callable = new CallCount(arr, 1, numberDec/2);
-
-        // Callable<Integer> callable = new CallCount(numberDec, arr);
 
         List<Callable<Integer>> calls = new ArrayList<>();
         calls.add(new CallCount(arr, 1, part));
@@ -59,35 +50,6 @@ public class AppCallLoop {
             System.out.println(f.get());
         }
 
-
-
-        // Callable<Integer> callable = new CallCount(arr, 1, part);
-        // Callable<Integer> callable2 = new CallCount(arr, part + 1, part * 2);
-        // Callable<Integer> callable3 = new CallCount(arr, (part * 2) + 1, part * 3);
-        // Callable<Integer> callable4 = new CallCount(arr, (part * 3) + 1, part * 4);
-        // Callable<Integer> callable5 = new CallCount(arr, (part * 4) + 1, part * 5);
-        // Callable<Integer> callable6 = new CallCount(arr, (part * 5) + 1, part * 6);
-        // Callable<Integer> callable7 = new CallCount(arr, (part * 6) + 1, part * 7);
-        // Callable<Integer> callable8 = new CallCount(arr, (part * 7) + 1, numberDec);
-        // Future<Integer> future = exec.submit(callable);
-        // Future<Integer> future2 = exec.submit(callable2);
-        // Future<Integer> future3 = exec.submit(callable3);
-        // Future<Integer> future4 = exec.submit(callable4);
-        // Future<Integer> future5 = exec.submit(callable5);
-        // Future<Integer> future6 = exec.submit(callable6);
-        // Future<Integer> future7 = exec.submit(callable7);
-        // Future<Integer> future8 = exec.submit(callable8);
-        // // diffenece = future.get();
-
-        // System.out.println(future.get());
-        // System.out.println(future2.get());
-        // System.out.println(future3.get());
-        // System.out.println(future4.get());
-        // System.out.println(future5.get());
-        // System.out.println(future6.get());
-        // System.out.println(future7.get());
-        // System.out.println(future8.get());
-
         System.out.println(new Date());
         exec.shutdown();
 
@@ -99,7 +61,6 @@ class CallCount implements Callable {
 
     private long[] arr;
     private  long diffenece = Long.MAX_VALUE;
-    // private  long diffenece = 0;
 
     private long sum0 = 0, sum1 = 0;
     private long start;
@@ -124,16 +85,7 @@ class CallCount implements Callable {
 
             long dif = Math.abs(sum1 - sum0);
 
-            // if(dif == 0) {
-            //     System.out.println("zero " + Thread.currentThread().getName());
-            //     System.out.println(new Date());
-            //     System.exit(0);
-            // }
-
             if (dif < diffenece) {
-                // if (dif > diffenece) {
-
-                // System.out.println(Thread.currentThread().getName() + " " + dif);
                 diffenece = dif;
             }
 
@@ -147,5 +99,3 @@ class CallCount implements Callable {
     }
 
 }
-
-// Runtime.getRuntime().availableProcessors();
--- src/main/java/AppKnapSack.java
+++ src/main/java/AppKnapSack.java
@@ -4,32 +4,19 @@ import java.util.Random;
 public class AppKnapSack {
 
     public static void main(String args[]) {
-//        int val[] = new int[]{60, 100, 120};
-//        int wt[] = new int[]{10, 20, 30};
-//        int W = 30;
-
-//        int val[] = {10, 40, 30, 50};
-//        int wt[] = {5, 4, 6, 3};
-//        int W = 10;
 
         int quantity = Integer.parseInt(args[0]);
-//        int quantity = 5;
         long[] arr = new long[quantity];
-//        long numberDec = (long)Math.pow(2, quantity) - 1;
-//        System.out.println(numberDec);
         long sum0 = 0, sum1 = 0;
         for (int i = 0; i < quantity; i++) {
             arr[i] = new Random().nextInt(10);
         }
-
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
 
-//        int val[] = {6, 9, 4, 1,4};
-//        int wt[] = {6, 9, 4, 1,4};
         long val[] = arr;
         long wt[] = arr;
         int sum = (int) Arrays.stream(arr).reduce(0, Long::sum);
@@ -37,18 +24,15 @@ public class AppKnapSack {
         int W = sum / 2;
         System.out.println("W " + W);
         int n = val.length;
-//        System.out.println(knapSack(W, wt, val, n));
         int one = knapSack(W, wt, val, n);
         System.out.println("one " + one);
         System.out.println("result " + (sum - (one * 2)));
     }
 
-    //    static int knapSack(int W, int wt[], int val[], int n) {
     static int knapSack(int W, long wt[], long val[], int n) {
         int i, w;
         int K[][] = new int[n + 1][W + 1];
 
-        // Build table K[][] in bottom up manner
         for (i = 0; i <= n; i++) {
             for (w = 0; w <= W; w++) {
                 if (i == 0 || w == 0)
